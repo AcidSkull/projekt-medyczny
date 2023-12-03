0b7d9543a94adff011d987da0321bca754965736
Patryk Jasik <patryk_jasik@o2.pl>
2023-12-03 20:16:31 +0100

    Its python time

--- backend/src/main/java/ADT/adt/controller/AppointmentController.java
+++
@@ -1,8 +0,0 @@
-package ADT.adt.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AppointmentController {
-
-}
--- backend/src/main/java/ADT/adt/repo/AppointmentRepo.java
+++
@@ -1,9 +0,0 @@
-package ADT.adt.repo;
-
-import ADT.adt.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppointmentRepo extends JpaRepository<Appointment, Long> {
-}
